8ead2481b1857e668ea28fb1674344ffcda13fa4
Augusto César Ferreira <gutocf@gmail.com>
2022-06-23 16:07:44 -0300

    first commit

---
+++ src/View/Helper/FontAwesomeHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gutocf\FontAwesome\View\Helper;
+
+use Cake\View\Helper;
+use Khill\FontAwesome\FontAwesome;;
+
+class FontAwesomeHelper extends Helper
+{
+    /**
+     * Generates icon HTML
+     *
+     * @param string $icon Icon name without "fa-" prefix
+     * @param string $style Icon style (fas, far, fal, fab)
+     */
+    public function icon(string $icon, string $style = 'fas'): string
+    {
+        $fa = new FontAwesome($icon, $style);
+
+        return $fa->__toString();
+    }
+
+    /**
+     * Generates fixed size icon HTML
+     *
+     * @param string $icon Icon name without "fa-" prefix
+     * @param string $style Icon style (fas, far, fal, fab)
+     */
+    public function fixedWidth(string $icon, string $style = 'fas'): string
+    {
+        $fa = new FontAwesome($icon, $style);
+
+        return $fa
+            ->fixedWidth()
+            ->__toString();
+    }
+}
---
+++ tests/TestCase/View/Helper/FontAwesomeHelperTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Gutocf\Test\FontAwesome\View\Helper;
+
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+use Gutocf\FontAwesome\View\Helper\FontAwesomeHelper;
+
+class FontAwesomeHelperTest extends TestCase
+{
+    private FontAwesomeHelper $helper;
+
+    public function setUp(): void
+    {
+        $this->helper = new FontAwesomeHelper(new View());
+    }
+
+    public function testIcon(): void
+    {
+        $this->assertEquals('<i class="fas fa-gear"></i>', $this->helper->icon('gear'));
+        $this->assertEquals('<i class="far fa-gear"></i>', $this->helper->icon('gear', 'far'));
+    }
+
+    public function testFixedWidth(): void
+    {
+        $this->assertEquals('<i class="fas fa-gear fa-fw"></i>', $this->helper->fixedWidth('gear'));
+        $this->assertEquals('<i class="far fa-gear fa-fw"></i>', $this->helper->fixedWidth('gear', 'far'));
+    }
+}
